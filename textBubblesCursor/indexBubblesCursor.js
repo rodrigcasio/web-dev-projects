2202fb9ba47bc00c7fd98d08c870fe9bf06026d9
Rodrigo Casio <rod.casio@outlook.com>
2025-06-15 21:41:49 -0600

    Create indexBubblesCursor.js

---
+++ textBubblesCursor/indexBubblesCursor.js
@@ -0,0 +1,5 @@
+// this code is used to create a cursor that interacts with letters on the screen
+drawText('This works like magic!');
+
+// bellow is the code that creates the cursor
+bounceBubbles();
